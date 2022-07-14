cddeb3c1fced2de6ef1776f93159ea7741abb85e
Christian Kjær <c-kj@mail.dk>
2022-07-14 17:26:37 +0200

    Added tilemap to ingame scene

---
+++ Assets/Scripts/SceneLoading/ClientGameLoadingScreen.cs
@@ -0,0 +1,22 @@
+
+public class ClientGameLoadingScreen : ClientLoadingScreen
+{
+
+    protected override void AddOtherPlayerProgressBar(ulong clientId, NetworkTrackerLoadingProgress progressTracker)
+    {
+        base.AddOtherPlayerProgressBar(clientId, progressTracker);
+        m_LoadingProgressBars[clientId].NameText.text = GetPlayerName(clientId);
+    }
+
+    protected override void UpdateOtherPlayerProgressBar(ulong clientId, int progressBarIndex)
+    {
+        base.UpdateOtherPlayerProgressBar(clientId, progressBarIndex);
+        m_LoadingProgressBars[clientId].NameText.text = GetPlayerName(clientId);
+    }
+
+    private string GetPlayerName(ulong clientId)
+    {
+        return "PlayerX";
+    }
+    
+}
